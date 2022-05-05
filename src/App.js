c8f63526f77865b0b129662669e286a34ca82140
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-05-06 03:12:04 +0500

    Show Todo (done) Edit Todo (continue)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import {Header} from './components/Header/Header.jsx';
 import {Counter} from './components/Counter/Counter.jsx'
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [singleTodo, setSingleTodo] = useState();
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!isEdit){
+      setTodoList([...todoList,singleTodo]);
+      setSingleTodo('');
+    }
+    else{
+      console.log('editing');
+      setIsEdit(false);
+      setSingleTodo('');
+    }
+  }
+
   return (
-    <div className="App">
+    <div>
+
       <div>
-        {/* <Header /> */}
-        <Counter />
+        <Header />
+        {/* <Counter /> */}
       </div>
+
+      {/* === Todo === */}
+      <main>
+        {/* === Todo Input === */}
+        <section>
+          <form action="" className='todo-form' onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" name="" placeholder='Enter todo here' value={singleTodo} onChange={(e) => setSingleTodo(e.target.value)}/>
+            <button type='submit'>
+              {!isEdit? 'Add': 'Update'}
+            </button>
+          </form>
+        </section>
+        {/* Todo Input */}
+        {/* === Todo List === */}
+        <section>
+          <ol className='todo-list'>
+          {todoList.map((item) =>
+            <li key={Math.random()}>
+              <div className='todo-item-value'>{item}</div>
+              <div className='todo-item-btns'>
+                <button onClick={(e) => {setIsEdit(true);setSingleTodo(item)}}>Edit</button>
+                <button>Delete</button>
+              </div>
+            </li>
+          )}
+          </ol>
+        </section>
+        {/* Todo List */}
+      </main>
+      {/* Todo */}
+
     </div>
   );
 }
