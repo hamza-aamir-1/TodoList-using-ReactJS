317c91ae21c1ed918a762127279782e717a75a8f
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-05-07 01:06:51 +0500

    Todo List Completed

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ function App() {
   const [todoList, setTodoList] = useState([]);
   const [singleTodo, setSingleTodo] = useState();
   const [isEdit, setIsEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!isEdit){
-      setTodoList([...todoList,singleTodo]);
-      setSingleTodo('');
-    }
-    else{
-      console.log('editing');
-      setIsEdit(false);
-      setSingleTodo('');
-    }
+    setTodoList([...todoList,singleTodo]);
+    setSingleTodo('');
+  }
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEdit(false);
+    const editedTodo = [...todoList];
+    editedTodo[editIndex] = singleTodo;
+    setTodoList(editedTodo)
+    setSingleTodo('');
+  }
+
+  const handleDelete = (item) => {
+    const deletedTodo = todoList.filter((todo) => item !== todo);
+    setTodoList(deletedTodo);
   }
 
   return (
@@ -28,13 +36,15 @@ function App() {
       <div>
         <Header />
         {/* <Counter /> */}
+        {console.log(todoList)}
       </div>
 
       {/* === Todo === */}
       <main>
         {/* === Todo Input === */}
         <section>
-          <form action="" className='todo-form' onSubmit={(e) => handleSubmit(e)}>
+          {/* <form action="" className='todo-form' onSubmit={(e) => handleSubmit(e)}> */}
+          <form action="" className='todo-form' onSubmit={(e) => !isEdit? handleSubmit(e): handleEdit(e)}>
             <input type="text" name="" placeholder='Enter todo here' value={singleTodo} onChange={(e) => setSingleTodo(e.target.value)}/>
             <button type='submit'>
               {!isEdit? 'Add': 'Update'}
@@ -49,8 +59,8 @@ function App() {
             <li key={Math.random()}>
               <div className='todo-item-value'>{item}</div>
               <div className='todo-item-btns'>
-                <button onClick={(e) => {setIsEdit(true);setSingleTodo(item)}}>Edit</button>
-                <button>Delete</button>
+                <button onClick={() => {setIsEdit(true);setSingleTodo(item);setEditIndex(todoList.indexOf(item))}}>Edit</button>
+                <button onClick={() => handleDelete(item)}>Delete</button>
               </div>
             </li>
           )}
